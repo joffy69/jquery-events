fa7b916a6c9aa835591da28d19c7e0c71dd93630
joffy69 <jcwgibbs@gmail.com>
2021-07-26 21:56:24 +0000

    latest video

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-var xhr=new XMLHttpRequest();
+7 lines (21 sloc)  632 Bytes
+  
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200){
-        document.getElementById('data').innerHTML = this.responseText;
-    }
-};
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-xhr.send();
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+
+        data.forEach(function(item) {
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
+    });
+}
